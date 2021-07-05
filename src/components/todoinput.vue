33550ac61d311709f4fd2df770f7bc88f011467f
qls2920 <qls2920@naver.com>
2021-07-05 15:11:34 +0900

    git

--- src/components/todoinput.vue
+++ src/components/todoinput.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        <input type="text" v-model="newTodoItem">
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="글자를 입력하세요." v-on:keyup.enter="addTodo">
+        <button class="addContainer" v-on:click="addTodo" >
+            <i class="addBtn fas fa-plus"></i>
+        </button>
     </div>
-    {{newTodoItem}}
+    
 
 </template>
 
@@ -11,12 +14,45 @@ export default {
     name:'input',
     data(){
         return{
-            newTodoItem:''
+            newTodoItem:'',
+        }
+    },
+    methods:{
+        addTodo(){
+            console.log(this.newTodoItem);
+            let value = this.newTodoItem;
+            // localStorage.setItem(value,value);
+            this.$emit('addTodo',value);
+            this.clearInput();
+        },
+        clearInput(){
+            this.newTodoItem = '';
         }
     }
 }
 </script>
 
 <style>
-
+    input:focus {outline:none;}
+    .inputBox {
+        background: #fff;
+        line-height: 50px;
+        height: 50px;
+        border-radius: 5px;
+        margin-bottom:10px;
+    }
+    .inputBox input{
+        border-style: none;
+        font-size:1rem;
+    }
+    .addContainer {
+        float: right;
+        background:linear-gradient(to right,#6478fb, #8763fb);
+        display: inline-block;
+        width: 3rem; height: 50px;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn {
+        color:white;
+    }
 </style>
